c2ea011217b74f85f5523f581134f8d75c212ad5
Akito <akito-artisan@five-foxes.com>
2012-05-19 16:32:04 +0900

    actionBase viewBase added

--- libs/envi3/task/plugin/init/app.php
+++ libs/envi3/task/plugin/init/app.php
@@ -19,12 +19,21 @@ if (!isset($argv[2])) {
 $project_name = $argv[2];
 
 $arr[] = $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR;
-$arr[] = $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR;
 $arr[] = $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR;
 $arr[] = $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR."libs".DIRECTORY_SEPARATOR;
+$arr[] = $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR."libs".DIRECTORY_SEPARATOR."controller".DIRECTORY_SEPARATOR;
 
+$ds = DIRECTORY_SEPARATOR;
+
+$default_config_dir = dirname(__FILE__)."{$ds}..{$ds}..{$ds}..{$ds}default_config{$ds}";
 
 foreach ($arr as $item) {
     mkdir($item);
     echo $item."\n";
 }
+
+$text = copy($task_plugin_dir.$module.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'actionBase.class.php', $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR."libs".DIRECTORY_SEPARATOR."controller".DIRECTORY_SEPARATOR.'actionBase.class.php');
+$text = copy($task_plugin_dir.$module.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'viewBase.class.php', $base_dir."apps".DIRECTORY_SEPARATOR.$project_name.DIRECTORY_SEPARATOR."libs".DIRECTORY_SEPARATOR."controller".DIRECTORY_SEPARATOR.'viewBase.class.php');
+copy($default_config_dir.'main.yml', $base_dir."config{$ds}".$project_name.'.yml');
+copy($default_config_dir.'main_databases.yml', $base_dir."config{$ds}".$project_name.'_databases.yml');
+copy($default_config_dir.'main_di_container.yml', $base_dir."config{$ds}".$project_name.'_di_container.yml');
---
+++ libs/envi3/task/plugin/init/data/viewBase.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package
+ * @subpackage
+ * @since 0.1
+ * @author
+ */
+
+
+/**
+ * @package
+ * @subpackage
+ * @since 0.1
+ * @author
+ */
+
+class viewBase extends EnviViewBase
+{
+
+
+}
