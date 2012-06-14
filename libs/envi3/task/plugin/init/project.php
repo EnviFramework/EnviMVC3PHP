5961d29ea9a79d9eca7bebdd0f4eae20fc82581d
Akito <akito-artisan@five-foxes.com>
2012-06-14 15:07:22 +0900

    タスクの追加

---
+++ libs/envi3/task/plugin/init/project.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *  プロジェクトの追加タスク
+ *
+ *
+ * PHP versions 5
+ *
+ *
+ * @category   MVC
+ * @package    Envi3
+ * @subpackage EnviMVCCore
+ * @author     Akito <akito-artisan@five-foxes.com>
+ * @copyright  2011-2012 Artisan Project
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    GIT: $Id:$
+ * @link       https://github.com/EnviMVC/EnviMVC3PHP
+ * @see        https://github.com/EnviMVC/EnviMVC3PHP/wiki
+ * @since      File available since Release 1.0.0
+ */
+
+umask(0);
+$arr[] = $base_dir."apps";
+$arr[] = $base_dir."cache";
+$arr[] = $base_dir."config";
+$arr[] = $base_dir."logs";
+$ds = DIRECTORY_SEPARATOR;
+foreach ($arr as $item) {
+    mkdir($item);
+    echo $item."\n";
+}
