ea0afdaac2c4a8490886923a3d86c9596f4eee48
Akito <akito-artisan@five-foxes.com>
2012-08-17 14:18:59 +0900

    edit user

--- libs/envi3/task/plugin/build/data/BasePeer.class.php
+++ libs/envi3/task/plugin/build/data/BasePeer.class.php
@@ -35,8 +35,118 @@
  */
 class Base%%class_name%%Peer
 {
-    public $table_name = '%%table_name%%';
+    /**
+     * テーブル名
+     *
+     * @access      protected
+     * @var         string
+     */
+    protected static $table_name = '%%table_name%%';
     
+    /**
+     * クエリリスト
+     *
+     * @access      protected
+     * @var         array
+     */
+    protected static $queries = array();
+    
+    /**
+     * +-- Suffix対応で一行抽出する
+     *
+     * @access      public
+     * @static
+     * @param       string $query_key
+     * @param       array $bind_array
+     * @param       string $suffix OPTIONAL:NULL
+     * @param       EnviDBIBase $con OPTIONAL:NULL
+     * @return      %%class_name%%|boolean
+     */
+    public static function retrieveWithSuffix($query_key, array $bind_array, $suffix = NULL, EnviDBIBase $con = NULL)
+    {
+        if (!isset(%%class_name%%Peer::$queries[$query_key])) {
+            throw new EnviException("{$query_key}は存在しません");
+        }
+        $dbi = $con ? $con : extension()->DBI()->getInstance('%%instance_name%%');
+        $sql = self::getReplacedSQL(%%class_name%%Peer::$queries[$query_key], $suffix);
+        
+        
+        $res = $dbi->getRow($sql, array($bind_array));
+        if (!$res) {
+            return false;
+        }
+        $ormap = new %%class_name%%;
+        $ormap->hydrate($res);
+        return $ormap;
+    }
+    /* ----------------------------------------- */
+    
+
+    /**
+     * +-- Suffix対応で複数行抽出する
+     *
+     * @access      public
+     * @static
+     * @param       string $query_key
+     * @param       array $bind_array
+     * @param       string $suffix OPTIONAL:NULL
+     * @param       EnviDBIBase $con OPTIONAL:NULL
+     * @return      array
+     */
+    public static function retrieveAllWithSuffix($query_key, array $bind_array, $suffix = NULL, EnviDBIBase $con = NULL)
+    {
+        if (!isset(%%class_name%%Peer::$queries[$query_key])) {
+            throw new EnviException("{$query_key}は存在しません");
+        }
+        $dbi = $con ? $con : extension()->DBI()->getInstance('%%instance_name%%');
+        $sql = self::getReplacedSQL(%%class_name%%Peer::$queries[$query_key], $suffix);
+        
+        
+        $res = $dbi->getAll($sql, array($bind_array));
+        if (!$res) {
+            return array();
+        }
+        foreach ($res as $key => $item) {
+            $res[$key] = new %%class_name%%;
+            $res[$key]->hydrate($item);
+        }
+        return $res;
+    }
+    /* ----------------------------------------- */
+    
+    /**
+     * +-- 複数行抽出する
+     *
+     * @access      public
+     * @static
+     * @param       string $query_key
+     * @param       array $bind_array
+     * @param       EnviDBIBase $con OPTIONAL:NULL
+     * @return      array
+     */
+    public static function retrieveAll($query_key, array $bind_array, EnviDBIBase $con = NULL)
+    {
+        return self::retrieveAllWithSuffix($query_key, $bind_array, NULL, $con)
+    }
+    /* ----------------------------------------- */
+    
+    /**
+     * +-- 一行抽出する
+     *
+     * @access      public
+     * @static
+     * @param       string $query_key
+     * @param       array $bind_array
+     * @param       EnviDBIBase $con OPTIONAL:NULL
+     * @return      %%class_name%%|boolean
+     */
+    public static function retrieve($query_key, array $bind_array, EnviDBIBase $con = NULL)
+    {
+        return self::retrieveWithSuffix($query_key, $bind_array, NULL, $con)
+    }
+    /* ----------------------------------------- */
+    
+
     /**
      * +-- PKで抽出
      *
@@ -71,7 +181,7 @@ class Base%%class_name%%Peer
      */
     final protected static function getReplacedSQL($sql, $suffix = NULL)
     {
-        $table_name = $this->table_name;
+        $table_name = self::$table_name;
         if ($suffix === NULL) {
             $table_name .= "_{$suffix}";
         }
