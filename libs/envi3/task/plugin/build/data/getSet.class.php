f9e6c6bc986f8cf2baf536a21a9c0bab14c8ba9a
Akito <akito-artisan@five-foxes.com>
2013-02-03 18:54:25 +0900

    bugfix

---
+++ libs/envi3/task/plugin/build/data/getSet.class.php
@@ -0,0 +1,12 @@
+
+    public function get%%method%%()
+    {
+        return isset($this->to_save['%%column%%']) ? $this->to_save['%%column%%'] : NULL;
+    }
+
+    public function set%%method%%($val)
+    {
+        $this->_is_modify = true;
+        $this->to_save['%%column%%'] = $val;
+    }
+
