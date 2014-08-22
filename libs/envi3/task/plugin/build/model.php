50437d101771a1ed1c34e34bb43fdfd975ba9493
suzunone <suzunone@localhost.localdomain>
2014-08-21 22:19:59 -0400

    DBのOracle対応

--- libs/envi3/task/plugin/build/model.php
+++ libs/envi3/task/plugin/build/model.php
@@ -79,47 +79,14 @@ foreach ($config['SCHEMA'] as $table_name => &$schema) {
     $use_apc_cache = $use_cache ? $schema['use_cache'] == 'apc' : false;
     $foreign_key = isset($schema['foreign_key']) ? $schema['foreign_key']: array();
 
-    // DBに接続して、自動的にスキーマ情報を取得する
-    if ($auto_schema) {
-        $schema['schema'] = array();
-        if (!isset($EnviDBInstance)) {
-            $EnviDBInstance = new EnviDBInstance($database_yaml);
-        }
-        $dbi = $EnviDBInstance->getInstance($instance_name);
-        $schema_arr = $dbi->getAll('desc '.$table_name);
-        $index_schema_arr = $dbi->getAll('SHOW INDEX FROM '.$table_name);
-
-        foreach ($schema_arr as $k => $arr) {
-            $schema['schema'][$arr['Field']]['type']    = $arr['Type'];
-            switch ($arr['Default']) {
-            case 'CURRENT_TIMESTAMP':
-                $arr['Default'] = NULL;
-            break;
-            default:
-                break;
-            }
-            $schema['schema'][$arr['Field']]['default'] = $arr['Default'];
-
-            foreach ($index_schema_arr as $index_arr) {
-                if ($index_arr['Column_name'] === $arr['Field']) {
-                    if ($index_arr['Non_unique'] == 0 && strtolower($index_arr['Key_name']) === 'primary') {
-                        $schema['schema'][$arr['Field']]['primary'] = $index_arr['Key_name'];
-                    } elseif ($index_arr['Non_unique'] == 0) {
-                        $schema['schema'][$arr['Field']]['unique'][] = $index_arr['Key_name'];
-                    } else {
-                        $schema['schema'][$arr['Field']]['index'][] = $index_arr['Key_name'];
-                    }
-                }
-            }
-            if (strtolower($arr['Null']) === 'no') {
-                $schema['schema'][$arr['Field']]['not_null'] = true;
-            }
-            if (strtolower($arr['Extra']) === 'auto_increment') {
-                $schema['schema'][$arr['Field']]['auto_increment'] = true;
-            }
-
-        }
+    $php_type = 'mysql';
+    if (!isset($schema['php_type'])) {
+        $schema['php_type'] = 'mysql';
     }
+
+    include dirname(__FILE__).DIRECTORY_SEPARATOR.'driver'.DIRECTORY_SEPARATOR.'autoScheme'.DIRECTORY_SEPARATOR.$schema['php_type'].'.php';
+
+
     $cache_hydrate = '';
     if ($use_apc_cache) {
         $cache_hydrate = file_get_contents($task_plugin_dir.$module.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'UseApcCache.php.snp');
